539b10bdfa91a350ba19d27bb1466f8f4e606d44
Rajasibii <srajasibi550@gmail.com>
2025-08-04 16:21:10 +0530

    backup before layout change

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Blog } from './blog/blog';
+import { Contact } from './contact/contact';
+import { AllCourse } from './all-course/all-course';
+import { Pages } from './pages/pages';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component:Home},
+    {path:'blog',component:Blog},
+    {path:'contact',component:Contact},
+    {path:'all-course', component:AllCourse},
+    {path:'pages',component:Pages}
+];
