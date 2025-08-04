bcad74f952d6c3e12b6a42670f8f9f3eeae61278
Rajasibii <srajasibi550@gmail.com>
2025-08-04 16:21:10 +0530

    backup before layout change

---
+++ src/app/all-course/all-course.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all-course',
+  imports: [],
+  templateUrl: './all-course.html',
+  styleUrl: './all-course.css'
+})
+export class AllCourse {
+
+}
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { CommonModule } from '@angular/common';
+import { Component, HostListener, ViewEncapsulation } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
+// import { Menu } from './shared/menu/menu';
+// import { AuthLinks } from './shared/auth-links/auth-links';
+// import { SocialLinks } from "./shared/social-links/social-links";
+// import { Email } from "./shared/email/email";
+// import { Phone } from "./shared/phone/phone";
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule, CommonModule,],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  encapsulation: ViewEncapsulation.None,
 })
 export class App {
-  protected title = 'my-angular-app';
+  isMenuScrolled = false;
+  isSidebarShowing = false;
+
+  @HostListener('window:scroll', ['$event'])
+  scrollCheck(event: Event) {
+    if (window.pageYOffset > 100) {
+      this.isMenuScrolled = true;
+    } else {
+      this.isMenuScrolled = false;
+    }
+  }
+  openSideBar() {
+    this.isSidebarShowing = true;
+  }
+  closeSideBar() {
+    this.isSidebarShowing = false;
+  }
 }
